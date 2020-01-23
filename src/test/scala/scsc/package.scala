f4176f5f6f7e5544e139a9a1bd8be9e03fe09fa8
rau <rauschie.j@gmail.com>
2020-01-23 21:04:49 +0100

    update compiler flags

--- src/test/scala/scsc/package.scala
+++ src/test/scala/scsc/package.scala
@@ -3,15 +3,14 @@ package object scsc {
   import scsc.CqlDataType.{BOOLEAN, DOUBLE, INT, TEXT}
   import scsc.ops.hlist.{FindByName, GetNames, GetTypes}
   import shapeless.{::, the, HNil}
-  import shapeless.ops.hlist.{Filter, Selector}
 
   type TestKey = String :: Int :: Double :: HNil
   type TestRecord = String :: Int :: Double :: Option[Boolean] :: HNil
   type TestPartitioning = Column["foo", TEXT] :: Column["bar", INT] :: HNil
-  val v=the[FindByName[TestPartitioning,"foo"]]
-  the[v.MappedTo<:<Column["foo",TEXT]]
-  val v1=the[FindByName[TestPartitioning,"bar"]]
-  the[v1.MappedTo<:<Column["bar",INT]]
+  val v = the[FindByName[TestPartitioning, "foo"]]
+  the[v.MappedTo <:< Column["foo", TEXT]]
+  val v1 = the[FindByName[TestPartitioning, "bar"]]
+  the[v1.MappedTo <:< Column["bar", INT]]
   //val v2=the[FindByName[TestPartitioning,"baz"]]
 
   type TestClustering = Column["baz", DOUBLE] :: HNil
