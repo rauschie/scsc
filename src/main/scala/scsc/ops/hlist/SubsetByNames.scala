4eff15fa56e2ef594b5e6c6624ed2591a3db8647
rau <rauschie.j@gmail.com>
2020-01-23 19:38:40 +0100

    rename RetainAll to Subset

---
+++ src/main/scala/scsc/ops/hlist/SubsetByNames.scala
@@ -0,0 +1,37 @@
+package scsc.ops.hlist
+
+import scsc.ops.BinaryTypeMapping
+import shapeless.{HList, ::, HNil}
+
+trait SubsetByNames[L, N] extends BinaryTypeMapping[L, N] {
+  type MappedTo <: HList
+}
+
+object SubsetByNames extends LowPrioritySubsetByNames {
+
+  import shapeless.the
+
+  type Aux[L, N, M <: HList] = SubsetByNames[L, N] {
+    type MappedTo = M
+  }
+  implicit def matching[L, N <: Singleton with String, T <: HList](
+      implicit ev: FindByName[L, N],
+      findNames: SubsetByNames[L, T]
+  ): Aux[L, N :: T, ev.MappedTo :: findNames.MappedTo] = new SubsetByNames[L, N :: T] {
+    type MappedTo = ev.MappedTo :: findNames.MappedTo
+  }
+  implicit def retainHNil[L]: Aux[L, HNil, HNil] = new SubsetByNames[L, HNil] {
+    type MappedTo = HNil
+  }
+}
+
+trait LowPrioritySubsetByNames {
+
+  import scsc.ops.hlist.SubsetByNames.Aux
+
+  implicit def skip[L, N <: Singleton with String, T <: HList](
+      implicit findNames: SubsetByNames[L, T]
+  ): Aux[L, N :: T, findNames.MappedTo] = new SubsetByNames[L, N :: T] {
+    type MappedTo = findNames.MappedTo
+  }
+}
--- src/test/scala/scsc/ops/hlist/SubsetByNamesTest.scala
+++ src/test/scala/scsc/ops/hlist/SubsetByNamesTest.scala
@@ -5,13 +5,13 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class RetainAllNamedTest extends AnyWordSpec {
+class SubsetByNamesTest extends AnyWordSpec {
   import shapeless.{the, HNil, ::}
-  "RetainAllNamed" when {
+  "SubsetByNames" when {
     "called on HNil" should {
       "return empty" in {
         import shapeless.{the, HNil}
-        val f = the[RetainAllNamed[HNil, "foo" :: HNil]]
+        val f = the[SubsetByNames[HNil, "foo" :: HNil]]
         the[f.MappedTo <:< HNil]
       }
     }
@@ -20,7 +20,7 @@ class RetainAllNamedTest extends AnyWordSpec {
         import scsc.TestPartitioning
         import scsc.Column
         import scsc.CqlType.TEXT
-        val f1 = the[RetainAllNamed[TestPartitioning, "foo" :: HNil]]
+        val f1 = the[SubsetByNames[TestPartitioning, "foo" :: HNil]]
         the[f1.MappedTo <:< (Column["foo", TEXT] :: HNil)]
       }
     }
@@ -29,7 +29,7 @@ class RetainAllNamedTest extends AnyWordSpec {
         import scsc.TestPartitioning
         import scsc.Column
         import scsc.CqlType._
-        val f1 = the[RetainAllNamed[TestPartitioning, "foo" :: "bar" :: HNil]]
+        val f1 = the[SubsetByNames[TestPartitioning, "foo" :: "bar" :: HNil]]
         the[f1.MappedTo <:< (Column["foo", TEXT] :: Column["bar", INT] :: HNil)]
       }
     }
