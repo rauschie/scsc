b2c452a60f23c367c572e0e4453351ac92afc41a
rau <rauschie.j@gmail.com>
2020-01-27 18:58:13 +0100

    make Put pass minimal test

--- src/main/scala/scsc/ops/crud/Put.scala
+++ src/main/scala/scsc/ops/crud/Put.scala
@@ -1,29 +1,22 @@
 package scsc.ops.crud
 
 import shapeless.HList
+import scsc.ops.hlist.{GetNames, ToStrings}
+import shapeless.BasisConstraint
 
-sealed trait Put[Kl, Cl] {
-  type Names <: HList
+sealed trait Put[Kl, Cl, Nl] {
 
   def getQuery(keySpaceName: String, tableName: String): String
 }
 
-object Put {
-//todo make this pass tests
-  import scsc.ops.hlist.{GetNames, ToStrings}
-  import shapeless.BasisConstraint
-  import shapeless.ops.hlist.Prepend
+object Put extends {
 
-  type Aux[Kl, Cl, Nl] = Put[Kl, Cl] {
-    type Names = Nl
-  }
-
-  implicit def put[Kl <: HList, Cl, Kn<:HList, Cn<:HList, Nl <: HList](
-      implicit keyNames: GetNames.Aux[Kl, Kn],
+  implicit def put[Kl <: HList, Cl, Kn <: HList, Cn <: HList, Nl <: HList](
+      implicit keyColumnNames: GetNames.Aux[Kl, Kn],
       columnNames: GetNames.Aux[Cl, Cn],
       ev: BasisConstraint[Kn, Nl],
       names: ToStrings[Nl]
-  ): Aux[Kl, Cl, Nl] = new Put[Kl, Cl] {
+  ): Put[Kl, Cl, Nl] = new Put[Kl, Cl, Nl] {
 
     type Names = Nl
 
@@ -31,15 +24,5 @@ object Put {
       s"INSERT INTO $tableName (${names().mkString(", ")}) " +
         s"VALUES (${names() map (_ => "?") mkString ", "})"
     }
-  }
-
-  implicit def put[Kl <: HList, Cl <: HList, Nl <: HList](
-      implicit keyColumnNames: GetNames[Kl],
-      columnNames: GetNames.Aux[Cl, Nl],
-      names: ToStrings[Nl]
-  ): Put[Kl, Cl] = new Put[Kl, Cl] {
-    type Names = Nl
-
-    def getQuery(keySpaceName: String, tableName: String): String = ???
   }
 }
