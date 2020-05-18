18e9a247a65987bed0c44e4cc8556f01b9d9c944
rau <jani.rauschenberger@hotmail.com>
2020-05-19 00:53:45 +0200

    implement Transaction monad stub

--- src/main/scala/scsc/Transaction.scala
+++ src/main/scala/scsc/Transaction.scala
@@ -1,5 +1,16 @@
 package scsc
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
+
 trait Transaction[A] {
-  //todo make this into a Monad
+  self =>
+  def run(implicit ec: ExecutionContext): Future[A]
+  def map[B](f: A => B): Transaction[B] = new Transaction[B] {
+    def run(implicit ec: ExecutionContext) = self.run map f
+  }
+  def flatMap[B](f: A => Transaction[B]): Transaction[B] = new Transaction[B] {
+    def run(implicit ec: ExecutionContext): Future[B] =
+      self.run flatMap (f(_).run)
+  }
 }
